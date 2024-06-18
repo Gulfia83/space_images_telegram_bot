a92da46e830d293b081c79d00f26ef178ba10c0f
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-06-18 20:15:07 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,4 +8,4 @@ token = os.environ['TELEGRAM_BOT_TOKEN']
 chat_id = os.environ['TELEGRAM_CHAT_ID']
 bot = telegram.Bot(token)
 
-bot.send_message(chat_id=chat_id, text='Hello!')
+bot.send_document(chat_id=chat_id, document=open('images/spaceX0.jpeg', 'rb'))
