c990d7d7eb4a11d3f2e046b0b47dd3568d6d5e29
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-06-19 16:04:22 +0300

    Update autopublish_images_bot.py

--- autopublish_images_bot.py
+++ autopublish_images_bot.py
@@ -14,7 +14,8 @@ def autopublish_images(token, chat_id, folder_name, frequensy_of_publication):
         for image_to_publish in image_names:
             image_path = os.path.join(folder_name, image_to_publish)
             bot = telegram.Bot(token)
-            bot.send_document(chat_id=chat_id, document=open(image_path, 'rb'))
+            with open(image_path, 'rb') as image_path:
+                bot.send_document(chat_id=chat_id, document=image_path)
 
             time.sleep(int(frequensy_of_publication) * 60 * 60)
 
@@ -24,7 +25,7 @@ if __name__ == '__main__':
     chat_id = os.environ['TELEGRAM_CHAT_ID']
     frequensy_of_publication = os.environ['FREQUENSY_OF_PUBLICATION']
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='receive name of the folder with images, default name "images"')
     parser.add_argument('--folder_name', help='Name of folder with images', default='images')
     args = parser.parse_args()
     folder_name = args.folder_name
