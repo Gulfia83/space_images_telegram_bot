fcc4fa60da7cad8585a995cf8ec0138d7bf3545b
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-06-19 16:05:36 +0300

    Update publish_images_bot.py

--- publish_images_bot.py
+++ publish_images_bot.py
@@ -6,7 +6,7 @@ import argparse
 
 
 def publish_image(token, chat_id, folder_name, image_name):
-    if image_name is None:
+    if not image_name:
         folder_path, folder_names, image_names = next(os.walk(folder_name))
         image_to_publish = random.choice(image_names)
 
@@ -15,7 +15,8 @@ def publish_image(token, chat_id, folder_name, image_name):
   
     image_path = os.path.join(folder_name, image_to_publish)
     bot = telegram.Bot(token)
-    bot.send_document(chat_id=chat_id, document=open(image_path, 'rb'))
+    with open(image_path, 'rb') as image_path:
+        bot.send_document(chat_id=chat_id, document=image_path)
 
 
 if __name__ == '__main__':
@@ -23,9 +24,9 @@ if __name__ == '__main__':
     token = os.environ['TELEGRAM_BOT_TOKEN']
     chat_id = os.environ['TELEGRAM_CHAT_ID']
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Receive folder with images (default value "images") and image name')
     parser.add_argument('--folder_name', help='Name of folder with images', default='images')
-    parser.add_argument('--image_name', help='Name of image to publish', default=None)
+    parser.add_argument('--image_name', help='Name of image to publish')
     args = parser.parse_args()
     folder_name = args.folder_name
     image_name = args.image_name
