176fcb79db26aa92a8048b86e821b15cd3b08547
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-06-19 11:27:23 +0300

    Create autopublish_bot.py

---
+++ autopublish_bot.py
@@ -0,0 +1,32 @@
+import telegram
+from dotenv import load_dotenv
+import os
+import random
+import argparse
+import time
+
+
+def autopublish_images(token, chat_id, folder_name, frequensy_of_publication):
+    while True:
+        folder_path, folder_names, image_names = next(os.walk(folder_name))
+        random.shuffle(image_names)
+
+        for image_to_publish in image_names:
+            image_path = os.path.join(folder_name, image_to_publish)
+            bot = telegram.Bot(token)
+            bot.send_document(chat_id=chat_id, document=open(image_path, 'rb'))
+
+            time.sleep(int(frequensy_of_publication) * 60 * 60)
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.environ['TELEGRAM_BOT_TOKEN']
+    chat_id = os.environ['TELEGRAM_CHAT_ID']
+    frequensy_of_publication = os.environ['FREQUENSY_OF_PUBLICATION']
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder_name', help='Name of folder with images', default='images')
+    args = parser.parse_args()
+    folder_name = args.folder_name
+
+    autopublish_images(token, chat_id, folder_name, frequensy_of_publication)
