b711178a70430a7e296f80ed5b1f06a99f98e90a
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-06-19 16:05:28 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,19 +4,7 @@ import argparse
 from common_scripts import download_image
 
 
-def fetch_spacex_images(launch_id=None):
-    if launch_id==None:
-        url_api_spaceX = 'https://api.spacexdata.com/v5/launches/latest'
-        response = requests.get(url_api_spaceX)
-        response.raise_for_status()
-        links = response.json()['links']['flickr']['original']
-  
-        for number, link in enumerate(links):
-            path = f'images/spaceX{number}.jpeg'
-            
-            download_image(link, path)
-
-    else:
+def fetch_spacex_images(launch_id='latest'):
         url_api_spaceX = f'https://api.spacexdata.com/v5/launches/{launch_id}'
         response = requests.get(url_api_spaceX)
         response.raise_for_status()
@@ -28,15 +16,12 @@ def fetch_spacex_images(launch_id=None):
             download_image(link, path)
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--id', help='ID Spacex launch')
+    parser = argparse.ArgumentParser(description="Takes spaceX launch ID, default value 'latest'")
+    parser.add_argument('--id', default= 'latest',help='ID Spacex launch')
     args = parser.parse_args()
     
-
-    try:
-        os.makedirs('images')
-    except FileExistsError:
-        pass
+    os.makedirs('images', exist_ok=True)
+    
     fetch_spacex_images(args.id)
 
 if __name__ == '__main__':
